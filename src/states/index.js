8a760c173d63329dde0f22a45db0462a651bb5db
asakarso <asaprameswaru@gmail.com>
2026-03-11 18:41:44 +0800

    refactor: add centralized states index

---
+++ src/states/index.js
@@ -0,0 +1,22 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authUserReducer from './authUser/reducer';
+import isPreloadReducer from './isPreload/reducer';
+import usersReducer from './users/reducer';
+import threadsReducer from './threads/reducer';
+import leaderboardsReducer from './leaderboards/reducer';
+import detailThreadReducer from './threadDetail/reducer';
+import { loadingBarReducer } from '@dimasmds/react-redux-loading-bar';
+
+const store = configureStore({
+  reducer: {
+    authUser: authUserReducer,
+    isPreload: isPreloadReducer,
+    users: usersReducer,
+    threads: threadsReducer,
+    detailThread: detailThreadReducer,
+    leaderboards: leaderboardsReducer,
+    loadingBar: loadingBarReducer,
+  },
+});
+
+export default store;
