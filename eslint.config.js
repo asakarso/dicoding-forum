ab3422133447c84904f90447ba2bd3e8dac3b260
asakarso <asaprameswaru@gmail.com>
2026-03-14 22:10:59 +0800

    chore: setup storybook dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,31 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import globals from 'globals';
 import pluginReact from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 import daStyle from 'eslint-config-dicodingacademy';
 
-export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
-    settings: {
-      react: {
-        version: '19.2.4',
-      },
+export default defineConfig([{
+  files: ['**/*.{js,mjs,cjs,jsx}'],
+  plugins: { js },
+  extends: ['js/recommended'],
+  languageOptions: { globals: globals.browser },
+  settings: {
+    react: {
+      version: '19.2.4',
     },
   },
-  pluginReact.configs.flat.recommended,
-  daStyle,
-  {
-    files: ['**/*.cy.{js,jsx}'],
-    plugins: {
-      cypress: pluginCypress,
+}, pluginReact.configs.flat.recommended, daStyle, {
+  files: ['**/*.cy.{js,jsx}'],
+  plugins: {
+    cypress: pluginCypress,
+  },
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...pluginCypress.environments.globals.globals,
     },
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...pluginCypress.environments.globals.globals,
-      },
-    },
-  }
-]);
+  },
+}, ...storybook.configs["flat/recommended"]]);
