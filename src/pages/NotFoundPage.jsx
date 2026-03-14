32a743188f0a718bdf7f1ce0f5456dcd75ba78bc
asakarso <asaprameswaru@gmail.com>
2026-03-14 19:17:12 +0800

    test: retry make test error

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -9,7 +9,7 @@ function NotFoundPage() {
       <Navbar/>
       <div className='flex flex-col items-center justify-center h-full gap-6'>
         <h1 className='text-4xl font-bold text-center text-primary'>404 NOT FOUND</h1>
-        <p>Halaman ini tidak tersedia!</p>
+        <p>Halaman ini tidak tersedia</p>
         <button type="button" onClick={() => navigate('/')} className='bg-primary p-2 text-white rounded'>Kembali ke Home</button>
       </div>
     </div>
