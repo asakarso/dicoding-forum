21347df914962a7ae2e2720cd0f710d16253457d
asakarso <asaprameswaru@gmail.com>
2026-03-14 17:59:07 +0800

    test: add e2e test for login flow

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,79 @@
+/**
+ * - Login spec
+ *   - should display login page correctly
+ *   - should display alert when email is empty
+ *   - should display alert when password is empty
+ *   - should display alert when email and password are wrong
+ *   - should display homepage when email and password are correct
+ */
+
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+
+  it('should display login page correctly', () => {
+    cy.visit('http://localhost:5173/');
+
+    // memverifikasi elemen yang harus tampak pada halaman login
+    cy.get('input[placeholder="Email"]').should('be.visible');
+    cy.get('input[placeholder="Password"]').should('be.visible');
+    cy.get('button')
+      .contains(/^Login$/)
+      .should('be.visible');
+  });
+
+  it('should display alert when email is empty', () => {
+    // klik tombol login tanpa mengisi email
+    cy.get('button').contains(/^Login$/).click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('"email" is not allowed to be empty');
+    });
+  });
+
+  it('should display alert when password is empty', () => {
+    // mengisi username
+    cy.get('input[placeholder="Email"]').type('testuser@gmail.com');
+
+    // klik tombol login tanpa mengisi password
+    cy.get('button').contains(/^Login$/).click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('"password" is not allowed to be empty');
+    });
+  });
+
+  it('should display alert when email and password are wrong', () => {
+    // mengisi email
+    cy.get('input[placeholder="Email"]').type('testuser@gmail.com');
+
+    // mengisi password yang salah
+    cy.get('input[placeholder="Password"]').type('wrong_password');
+
+    // menekan tombol Login
+    cy.get('button').contains(/^Login$/).click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Email or password is wrong');
+    });
+  });
+
+  it('should display homepage when email and password are correct', () => {
+    // mengisi email
+    cy.get('input[placeholder="Email"]').type('test2026@gmail.com');
+
+    // mengisi password
+    cy.get('input[placeholder="Password"]').type('password123');
+
+    // menekan tombol Login
+    cy.get('button').contains(/^Login$/).click();
+
+    // memverifikasi bahwa elemen yang berada di homepage ditampilkan\
+    cy.get('main').contains(/^Lihat Diskusi Terbaru$/).should('be.visible');
+    cy.get('p').contains('Log Out').should('be.visible');
+  });
+});
